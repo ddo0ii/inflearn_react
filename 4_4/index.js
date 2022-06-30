07775b544d60d1edd6ee20c77509f30c13f80b95
soyhandong <21500035@handong.edu>
2022-06-30 09:28:54 +0900

    randomcolor모듈 사용

--- 4_4/index.js
+++ 4_4/index.js
@@ -1,1 +1,8 @@
-console.log("hello nodejs package");
+// node_modules로부터 다운로드 된 외부 모듈은 경로명시를 하지않고 아래와 같이만 입력해도 된다.
+const randomColor = require('randomcolor');
+
+let color1 = randomColor();
+let color2 = randomColor();
+let color3 = randomColor();
+
+console.log(color1, color2, color3);

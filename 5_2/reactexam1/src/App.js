b30ab6ca081724cb452a26a904b561c97eadb721
soyhandong <21500035@handong.edu>
2022-07-01 08:20:43 +0900

    MyHeader, MyFooter 추가

--- 5_2/reactexam1/src/App.js
+++ 5_2/reactexam1/src/App.js
@@ -1,12 +1,16 @@
 import './App.css';
+import MyHeader from './MyHeader';
+import MyFooter from './MyFooter';
 
 function App() {
   let name = "soyeong"
   return (
     <div className="App">
+      <MyHeader />
       <header className="App-header">
         <h2>Hello React! {name}</h2>
       </header>
+      <MyFooter />
     </div>
   );
 }

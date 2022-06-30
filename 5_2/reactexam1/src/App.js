b9061c6fe1bad7d411b4c2a6de33f139bc5fe6f9
soyhandong <21500035@handong.edu>
2022-07-01 08:30:53 +0900

    reactFragment설명

--- 5_2/reactexam1/src/App.js
+++ 5_2/reactexam1/src/App.js
@@ -2,6 +2,19 @@ import React from "react";
 import './App.css';
 import MyHeader from './MyHeader';
 
+/**
+컴포넌트 반환하려면, 최상위에서 무조건 묶어줘야한다.
+
+최상위 컴포넌트를 사용하지 않으려면
+
+import React from 'react'; 를하고,
+
+<React.Fragment></React.Fragment> 로 감싸주면 된다.
+
+귀찮으면 <></>로 빈태그로 감싸주면된다.
+
+React 기능 사용하지 않으면, React import할 필요가 없다. 
+ */
 function App() {
   let name = "soyeong"
   return (

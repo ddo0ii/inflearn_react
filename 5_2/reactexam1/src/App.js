6cf8df5cd033ca28a9abb6c4d53e545dab906478
soyhandong <21500035@handong.edu>
2022-07-01 08:42:53 +0900

    inline 스타일링

--- 5_2/reactexam1/src/App.js
+++ 5_2/reactexam1/src/App.js
@@ -1,14 +1,27 @@
 import React from "react";
-import './App.css';
+// import './App.css';
 import MyHeader from './MyHeader';
 
 function App() {
   let name = "soyeong"
+
+  const style = {
+    App: {
+      backgroundColor: "black"
+    },
+    h2: {
+      color: "red"
+    },
+    bold_text: {
+     color: "green"
+    }
+  }
+
   return (
-    <div className="App">
+    <div style={style.App}>
       <MyHeader />
-      <h2>Hello React! {name}</h2>
-      <b id="bold_text">React.js</b>
+      <h2 style={style.h2}>Hello React! {name}</h2>
+      <b style={style.bold_text} id="bold_text">React.js</b>
     </div>
   );
 }

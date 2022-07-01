79d725294f04ab4ab122296ebc5609ecc8f88b39
soyhandong <21500035@handong.edu>
2022-07-01 16:42:51 +0900

    state 설정하기

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -1,31 +1,39 @@
 import { useState } from "react";
 
 const DiaryEditor = () => {
-    // author는 state, setAuthor는 상태변화를 주도하는 함수
-    const [author, setAuthor] = useState("");
-    const [content, setContent] = useState("");
-    return (
-        <div className="DiaryEditor">
-            <h2>오늘의 일기</h2>
-            <div>
-                <input
-                    name="author"
-                    value={author}
-                    // 값이 바뀌었을 때
-                    onChange={(e) => {
-                        setAuthor(e.target.value);
-                    }}
-                />
-            </div>
-            <div>
-                <textarea
-                    value={content}
-                    onChange={(e) => {
-                        setContent(e.target.value);
-                    }}
-                />
-            </div>
-        </div>
-    );
+  // state는 state, setState는 상태변화를 주도하는 함수
+  const [state, setState] = useState({
+    author: "",
+    content: "",
+  });
+  return (
+    <div className="DiaryEditor">
+      <h2>오늘의 일기</h2>
+      <div>
+        <input
+          name="author"
+          value={state.author}
+          // 값이 바뀌었을 때
+          onChange={(e) => {
+              setState({
+                  author: e.target.value,
+                  content: state.content
+              });
+          }}
+        />
+      </div>
+      <div>
+        <textarea
+          value={state.content}
+          onChange={(e) => {
+            setState({
+              content: e.target.value,
+              author: state.author,
+            });
+          }}
+        />
+      </div>
+    </div>
+  );
 };
 export default DiaryEditor;

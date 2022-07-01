3795dd68ef58b18c5615d8dc27b72ffeae14ce07
soyhandong <21500035@handong.edu>
2022-07-01 15:56:15 +0900

    ReactDiary만들기

---
+++ simplediary/src/DiaryEditor.js
@@ -0,0 +1,25 @@
+import { useState } from "react";
+
+const DiaryEditor = () => {
+    // author는 state, setAuthor는 상태변화를 주도하는 함수
+    const [author, setAuthor] = useState("이정환");
+
+    return (
+        <div className="DiaryEditor">
+            <h2>오늘의 일기</h2>
+            <div>
+                <input
+                    name="author"
+                    value={author}
+                    // 값이 바뀌었을 때
+                    onChange={(e) => {
+                        console.log(e.target.value);
+                        console.log(e.target.name);
+                        setAuthor(e.target.value);
+                    }}
+                />
+            </div>
+        </div>
+    );
+};
+export default DiaryEditor;

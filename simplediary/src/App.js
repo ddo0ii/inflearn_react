43ec69833bef00299ee0ca645cda6ec175fa9d54
soyhandong <21500035@handong.edu>
2022-07-04 11:57:40 +0900

    DiaryList, DiaryItem

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,10 +1,37 @@
-import './App.css';
+import React, {useState, useEffect} from 'react';
 import DiaryEditor from './DiaryEditor';
+import DiaryList from './DiaryList';
+import "./App.css";
 
-function App() {
+const dummyList = [
+  {
+    id: 1,
+    author: "이정환",
+    content: "하이 1",
+    emotion: 5,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 2,
+    author: "홍길동",
+    content: "하이 2",
+    emotion: 2,
+    created_date: new Date().getTime(),
+  },
+  {
+    id: 3,
+    author: "아무개",
+    content: "하이 3",
+    emotion: 3,
+    created_date: new Date().getTime(),
+  },
+];
+
+const App = () =>  {
   return (
     <div className="App">
       <DiaryEditor />
+      <DiaryList diaryList={dummyList} />
     </div>
   );
 }
---
+++ simplediary/src/DiaryList.js
@@ -0,0 +1,22 @@
+import DiaryItem from "./DiaryItem";
+
+const DiaryList = ({ diaryList }) => {
+  return (
+    <div className="DiaryList">
+      <h2>일기 리스트</h2>
+      <h4>{diaryList.length}개의 일기가 있습니다.</h4>
+      <div>
+        {diaryList.map((it) => (
+          <DiaryItem key={it.id} {...it} />
+        ))}
+      </div>
+    </div>
+  );
+};
+
+// defaultProps는 undefined로 전달 될 수 있는 값을 default로 설정해주는 것
+DiaryList.defaultProps = {
+    diaryList: []
+}
+
+export default DiaryList;

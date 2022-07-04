2bcdc121f04e158fcfe640cb4573e6ce50d3a022
soyhandong <21500035@handong.edu>
2022-07-04 10:27:25 +0900

    DiaryEditor-select, submit

--- simplediary/src/DiaryEditor.js
+++ simplediary/src/DiaryEditor.js
@@ -5,16 +5,20 @@ const DiaryEditor = () => {
   const [state, setState] = useState({
     author: "",
     content: "",
+    emotion: 1,
   });
     
-    const handleChangeState = (e) => {
-        console.log(e.target.name);
-        console.log(e.target.value);
-        setState({
-            ...state,
-            [e.target.name]:e.target.value,
-        })
-    }
+  const handleChangeState = (e) => {
+    setState({
+      ...state,
+      [e.target.name]: e.target.value,
+    })
+  }
+  
+  const handleSubmit = () => {
+    console.log(state);
+    alert("저장 성공");
+  }
     
   return (
     <div className="DiaryEditor">
@@ -32,6 +36,18 @@ const DiaryEditor = () => {
             value={state.content} 
             onChange={handleChangeState} 
         />
+      </div>
+      <div>
+        <select name="emotion" value={state.emotion} onChange={handleChangeState}>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+        </select>
+      </div>
+      <div>
+        <button onClick={handleSubmit}>일기 저장하기</button>
       </div>
     </div>
   );

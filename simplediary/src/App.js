af0b1980f76a4d510b2a630c2ede14b4ece36c8d
soyhandong <21500035@handong.edu>
2022-07-05 17:11:18 +0900

    API 호출하기

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -1,13 +1,38 @@
-import { useRef, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import Lifecycle from "./Lifecycle";
+
+// https://jsonplaceholder.typicode.com/comments
 
 const App = () => {
   // 전역적으로 Data 관리할 state
   const [data, setData] = useState([]);
   const dataId = useRef(0);
+  // data 호출
+  const getData = async () => {
+    const res = await fetch(
+      "https://jsonplaceholder.typicode.com/comments"
+    ).then((res) => res.json());
+
+    const initData = res.slice(0, 20).map((it) => {
+      return {
+        author: it.email,
+        content: it.body,
+        emotion: Math.floor(Math.random() * 5) + 1,
+        created_date: new Date().getTime(),
+        id: dataId.current++,
+      };
+    });
+    setData(initData);
+  };
+
+  // App component가 mount 되자마자 호출해보자
+  // 빈배열 함수를 전달하면 바로 콜백함수를 마운트 되는 시점에 실행되게 된다.
+  // getData()로 API 호출
+  useEffect(() => {
+    getData();
+  }, []);
 
   const onCreate = (author, content, emotion) => {
     const created_date = new Date().getTime();
@@ -39,7 +64,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <Lifecycle />
       <DiaryEditor onCreate={onCreate} />
       <DiaryList onEdit={onEdit} onRemove={onRemove} diaryList={data} />
     </div>

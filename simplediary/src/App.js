dcd50fd77f450249fcf29f8ab15b84d06e944cd2
soyhandong <21500035@handong.edu>
2022-07-07 09:03:27 +0900

    연산 최적화 파일 삭제

--- simplediary/src/App.js
+++ simplediary/src/App.js
@@ -2,7 +2,6 @@ import { useMemo, useEffect, useRef, useState } from "react";
 import "./App.css";
 import DiaryEditor from "./DiaryEditor";
 import DiaryList from "./DiaryList";
-import OptimizeTest from "./OptimizeTest";
 
 const App = () => {
   // 전역적으로 Data 관리할 state
@@ -72,7 +71,6 @@ const App = () => {
 
   return (
     <div className="App">
-      <OptimizeTest />
       <DiaryEditor onCreate={onCreate} />
       <div>전체 일기 : {data.length}</div>
       <div>기분 좋은 일기 개수 : {goodCount}</div>

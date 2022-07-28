566654dba8076f8ac3140b10c21b2bf6d4f0ed06
soyhandong <21500035@handong.edu>
2022-07-28 23:09:12 +0900

    Page Routing

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import Home from './pages/Home'
+import New from './pages/New'
+import Edit from './pages/Edit'
+import Diary from './pages/Diary'
 
 function App() {
   return (
-    <div className="App">
-      <h2>App.js</h2>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <h2>App.js</h2>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/new" element={<New />} />
+          <Route path="/edit" element={<Edit />} />
+          <Route path="/diary" element={<Diary />} />
+        </Routes>
+      </div>
+    </BrowserRouter>
   );
 }
 

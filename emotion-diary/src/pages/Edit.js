65ba071257fb84f66e202df2abc7f3b145ce65ea
soyhandong <21500035@handong.edu>
2022-07-30 22:52:55 +0900

    useSearchParams

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,8 +1,21 @@
+import { useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+
+    const id = searchParams.get("id");
+    console.log("id: ", id);
+
+    const mode = searchParams.get("mode");
+    console.log("mode: ", mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이곳은 일기 수정 페이지입니다.</p>
+            <button onClick={() => setSearchParams({ who: "winterload" })}>
+                QS 바꾸기
+            </button>
         </div>
     )
 }

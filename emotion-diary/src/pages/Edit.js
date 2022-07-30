522c029a3f4e5816e9552ed43604f5236c477f67
soyhandong <21500035@handong.edu>
2022-07-30 23:03:39 +0900

    useNavigate

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,6 +1,7 @@
-import { useSearchParams } from "react-router-dom";
+import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
+    const navigate = useNavigate();
     const [searchParams, setSearchParams] = useSearchParams();
 
     const id = searchParams.get("id");
@@ -15,6 +16,18 @@ const Edit = () => {
             <p>이곳은 일기 수정 페이지입니다.</p>
             <button onClick={() => setSearchParams({ who: "winterload" })}>
                 QS 바꾸기
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/home");
+                }}
+            >
+                HOME으로 가기
+            </button>
+            <button onClick={() => {
+                navigate(-1);
+            }}>
+                뒤로가기
             </button>
         </div>
     )

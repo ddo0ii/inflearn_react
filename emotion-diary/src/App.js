8e25d25d50442a8056b81c7129965fe6f5b12dac
soyhandong <21500035@handong.edu>
2022-08-01 23:31:59 +0900

    MyButton

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -6,11 +6,33 @@ import New from './pages/New'
 import Edit from './pages/Edit'
 import Diary from './pages/Diary'
 
+// COMPONENTS
+import MyButton from './components/MyButton';
+
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>    
+        <h2>App.js</h2>   
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"positive"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"negative"}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+        />
+        <MyButton
+          text={"버튼"}
+          onClick={() => alert("버튼 클릭")}
+          type={"strangebutton"}
+        />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />

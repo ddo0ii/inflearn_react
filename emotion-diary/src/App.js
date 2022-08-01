35a2b35dea1e5385c56f2240c49928dc5cbfb352
soyhandong <21500035@handong.edu>
2022-08-01 22:52:53 +0900

    초기화

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -7,20 +7,10 @@ import Edit from './pages/Edit'
 import Diary from './pages/Diary'
 
 function App() {
-  const env = process.env;
-  env.PUBLIC_URL = env.PUBLIC_URL || "";
-
   return (
     <BrowserRouter>
       <div className="App">
-        <h2>App.js</h2>
-
-        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
-        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
-        
+        <h2>App.js</h2>    
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />

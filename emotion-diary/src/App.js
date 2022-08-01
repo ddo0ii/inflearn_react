2a8a8be26b15e32515940ff464b244a3bf5ab1a2
soyhandong <21500035@handong.edu>
2022-08-01 22:43:37 +0900

    이미지 에셋 세팅

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -7,10 +7,20 @@ import Edit from './pages/Edit'
 import Diary from './pages/Diary'
 
 function App() {
+  const env = process.env;
+  env.PUBLIC_URL = env.PUBLIC_URL || "";
+
   return (
     <BrowserRouter>
       <div className="App">
         <h2>App.js</h2>
+
+        <img src={process.env.PUBLIC_URL + `/assets/emotion1.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion2.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion3.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion4.png`} />
+        <img src={process.env.PUBLIC_URL + `/assets/emotion5.png`} />
+        
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/new" element={<New />} />

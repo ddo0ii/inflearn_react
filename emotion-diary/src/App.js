5979b9d9b140a0acae522bc908e2607e802964fb
soyhandong <21500035@handong.edu>
2022-08-09 16:06:39 +0900

    프로젝트 기초공사 1

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -8,11 +8,21 @@ import Diary from './pages/Diary'
 
 // COMPONENTS
 import MyButton from './components/MyButton';
+import MyHeader from './components/MyHeader';
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
+        <MyHeader
+          headText={"App"}
+          leftChild={
+            <MyButton text={"왼쪽 버튼"} onClick={() => alert("왼쪽 클릭")} />
+          }
+          rightChild={
+            <MyButton text={"오른쪽 버튼"} onClick={() => alert("오른쪽 클릭")} />
+          }
+        />
         <h2>App.js</h2>   
         <MyButton
           text={"버튼"}

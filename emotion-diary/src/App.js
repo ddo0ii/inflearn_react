cbe0aea84747d363509708045bb46efe36658a94
soyhandong <21500035@handong.edu>
2022-08-10 15:30:52 +0900

    프로젝트 Dispatch context 세팅하기

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -38,6 +38,7 @@ const reducer = (state, action) => {
 }
 
 export const DiaryStateContext = React.createContext();
+export const DiaryDispatchContext = React.createContext();
 
 function App() {
   const [data, dispatch] = useReducer(reducer, []);
@@ -76,17 +77,22 @@ function App() {
   };
   
   return (
+    
     <DiaryStateContext.Provider value={data}>
-      <BrowserRouter>
-        <div className="App">
-          <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/new" element={<New />} />
-            <Route path="/edit" element={<Edit />} />
-            <Route path="/diary/:id" element={<Diary />} />
-          </Routes>
-        </div>
-      </BrowserRouter>      
+      <DiaryDispatchContext.Provider
+        value={{ onCreate, onEdit, onRemove }}
+      >
+        <BrowserRouter>
+          <div className="App">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/new" element={<New />} />
+              <Route path="/edit" element={<Edit />} />
+              <Route path="/diary/:id" element={<Diary />} />
+            </Routes>
+          </div>
+        </BrowserRouter>      
+      </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
   );
 }

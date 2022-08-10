a009f97465b058717faa7af356a56a18f6a242cc
soyhandong <21500035@handong.edu>
2022-08-10 22:48:41 +0900

    년도와 월 들고오기 getFullYear, getMonth사용

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
 
+import MyHeader from "./../components/MyHeader"
+
 const Home = () => {
     // 날짜를 저장하는 state (기본값 - 현재시간)
     const [curDate, setCurDate] = useState(new Date());
     console.log(curDate);
+    // 월은 +1씩 해줘야함
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1} 월`
 
     return (
-        <div></div>
+        <div>
+            <MyHeader headText={headText} />
+        </div>
     )
 }
 

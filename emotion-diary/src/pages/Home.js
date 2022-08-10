22e975cb8bd2f5d1d7e00890caef019af50d8e1d
soyhandong <21500035@handong.edu>
2022-08-10 22:53:57 +0900

    버튼만들기

--- emotion-diary/src/pages/Home.js
+++ emotion-diary/src/pages/Home.js
@@ -1,19 +1,24 @@
 import { useState } from "react";
 
-import MyHeader from "./../components/MyHeader"
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
 
 const Home = () => {
-    // 날짜를 저장하는 state (기본값 - 현재시간)
-    const [curDate, setCurDate] = useState(new Date());
-    console.log(curDate);
-    // 월은 +1씩 해줘야함
-    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1} 월`
+  // 날짜를 저장하는 state (기본값 - 현재시간)
+  const [curDate, setCurDate] = useState(new Date());
+  console.log(curDate);
+  // 월은 +1씩 해줘야함
+  const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1} 월`;
 
-    return (
-        <div>
-            <MyHeader headText={headText} />
-        </div>
-    )
-}
+  return (
+    <div>
+      <MyHeader
+        headText={headText}
+        leftChild={<MyButton text={"<"} onClick={() => {}} />}
+        rightChild={<MyButton text={">"} onClick={()=>{}} />}
+      />
+    </div>
+  );
+};
 
 export default Home;

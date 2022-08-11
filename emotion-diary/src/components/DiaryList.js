a08480a357eb1bca297349f1a639262b25353a26
soyhandong <21500035@handong.edu>
2022-08-11 22:42:06 +0900

    감정별 필터링 씌우기

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -4,6 +4,13 @@ const sortOptionList = [
   { value: "latest", name: "최신순" },
   { value: "oldest", name: "오래된순" },
 ];
+
+const filterOptionList = [
+  { value: "all", name: "전부 다" },
+  { value: "good", name: "좋은 감정만" },
+  { value: "bad", name: "안 좋은 감정만" },
+];
+
 // value - select가 어떤것을 선택하고 있는지
 // onChange - select가 변경했을 때 바꿀 함수
 // optionList - select 안에 들어갈 option
@@ -22,8 +29,17 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 const DiaryList = ({ diaryList }) => {
   // 정렬 (초기값 - latest)
   const [sortType, setSortType] = useState("latest");
+  const [filter, setFilter] = useState("all");
 
   const getProcessedDiaryList = () => {
+    const filterCallBack = (item) => {
+      if (filter === "good") {
+        return parseInt(item.emotion) <= 3;
+      } else {
+        return parseInt(item.emotion) > 3;
+      }
+    };
+
     const compare = (a, b) => {
       if (sortType === "latest") {
         return parseInt(b.date) - parseInt(a.date);
@@ -35,9 +51,11 @@ const DiaryList = ({ diaryList }) => {
     // 그래서 다시 parse를 하면 다시 복호화 시켜준다!
     // 이렇게 하고 copyList에 넣어주는 것이다
     // 그래서 값만 건드리고 원본은 건드리지 않고 수행했음을 알 수 있다
-      const copyList = JSON.parse(JSON.stringify(diaryList));
-      const sortedList = copyList.sort(compare);
-      return sortedList;
+    const copyList = JSON.parse(JSON.stringify(diaryList));
+    const filterLists =
+      filter === "all" ? copyList : copyList.filter((it) => filterCallBack(it));
+    const sortedList = filterLists.sort(compare);
+    return sortedList;
   };
 
   return (
@@ -47,8 +65,15 @@ const DiaryList = ({ diaryList }) => {
         onChange={setSortType}
         optionList={sortOptionList}
       />
+      <ControlMenu
+        value={filter}
+        onChange={setFilter}
+        optionList={filterOptionList}
+      />
       {getProcessedDiaryList().map((it) => (
-        <div key={it.id}>{it.content}</div>
+        <div key={it.id}>
+          {it.content} {it.emotion}
+        </div>
       ))}
     </div>
   );

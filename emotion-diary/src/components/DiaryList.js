f96ee1a2cd9276ab76738f3e815d838112b71b96
soyhandong <21500035@handong.edu>
2022-08-11 22:48:15 +0900

    새 일기쓰기 페이지로 이동 버튼 생성

--- emotion-diary/src/components/DiaryList.js
+++ emotion-diary/src/components/DiaryList.js
@@ -1,4 +1,6 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import MyButton from "./MyButton";
 
 const sortOptionList = [
   { value: "latest", name: "최신순" },
@@ -27,6 +29,7 @@ const ControlMenu = ({ value, onChange, optionList }) => {
 };
 
 const DiaryList = ({ diaryList }) => {
+  const navigate = useNavigate();
   // 정렬 (초기값 - latest)
   const [sortType, setSortType] = useState("latest");
   const [filter, setFilter] = useState("all");
@@ -69,6 +72,11 @@ const DiaryList = ({ diaryList }) => {
         value={filter}
         onChange={setFilter}
         optionList={filterOptionList}
+      />
+      <MyButton
+        type={"positive"}
+        text={"새 일기쓰기"}
+        onClick={() => navigate("/new")}
       />
       {getProcessedDiaryList().map((it) => (
         <div key={it.id}>

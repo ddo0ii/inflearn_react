a22b50b7a602c5e7d0e6107959a1ef1f779d73c2
soyhandong <21500035@handong.edu>
2022-08-16 17:49:20 +0900

    toISOString를 사용하여 현 date useState로 삽입

--- emotion-diary/src/pages/New.js
+++ emotion-diary/src/pages/New.js
@@ -1,9 +1,17 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 import MyHeader from "./../components/MyHeader";
 import MyButton from "./../components/MyButton";
 
+const getStringDate = (date) => {
+  return date.toISOString().slice(0, 10);
+};
+
 const New = () => {
+  // 초기 값을 toISOString로 현 date를 잘라서 넣어주면 된다.
+  const [date, setDate] = useState(getStringDate(new Date()));
+
   const navigate = useNavigate();
   return (
     <div>
@@ -13,6 +21,19 @@ const New = () => {
           <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
         }
       />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input-box">
+            <input
+              className="input-date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+      </div>
     </div>
   );
 };

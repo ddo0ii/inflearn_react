c34886581a1a74faba2acbbdf6548be7548a8991
soyhandong <21500035@handong.edu>
2022-08-16 17:09:21 +0900

    NewPage Header 구현 버튼 및 네비게이트

--- emotion-diary/src/pages/New.js
+++ emotion-diary/src/pages/New.js
@@ -1,10 +1,20 @@
+import { useNavigate } from "react-router-dom";
+
+import MyHeader from "./../components/MyHeader";
+import MyButton from "./../components/MyButton";
+
 const New = () => {
-    return (
-        <div>
-            <h1>New</h1>
-            <p>이곳은 일기 작성 페이지입니다.</p>
-        </div>
-    )
-}
+  const navigate = useNavigate();
+  return (
+    <div>
+      <MyHeader
+        headText={"새 일기쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+      />
+    </div>
+  );
+};
 
 export default New;

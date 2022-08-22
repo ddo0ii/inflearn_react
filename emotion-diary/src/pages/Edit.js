fa02b4ec90a9c337cc92744a1e0865656a793ef6
soyhandong <21500035@handong.edu>
2022-08-22 22:43:17 +0900

    Edit 시작하기

--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,36 +1,13 @@
 import { useNavigate, useSearchParams } from "react-router-dom";
 
 const Edit = () => {
-    const navigate = useNavigate();
-    const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
-    const id = searchParams.get("id");
-    console.log("id: ", id);
-
-    const mode = searchParams.get("mode");
-    console.log("mode: ", mode);
-
-    return (
-        <div>
-            <h1>Edit</h1>
-            <p>이곳은 일기 수정 페이지입니다.</p>
-            <button onClick={() => setSearchParams({ who: "winterload" })}>
-                QS 바꾸기
-            </button>
-            <button
-                onClick={() => {
-                    navigate("/home");
-                }}
-            >
-                HOME으로 가기
-            </button>
-            <button onClick={() => {
-                navigate(-1);
-            }}>
-                뒤로가기
-            </button>
-        </div>
-    )
-}
+  return (
+    <div>
+      <h2>Edit</h2>
+    </div>
+  );
+};
 
 export default Edit;

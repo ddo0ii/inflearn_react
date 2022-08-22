38de31585f6d20800b034c36a9dca9971ed2ebd2
soyhandong <21500035@handong.edu>
2022-08-23 00:13:33 +0900

    Editor마무리

--- emotion-diary/src/App.js
+++ emotion-diary/src/App.js
@@ -1,20 +1,20 @@
-import React, { useReducer, useRef } from 'react';
+import React, { useReducer, useRef } from "react";
 
-import './App.css';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
-import Home from './pages/Home'
-import New from './pages/New'
-import Edit from './pages/Edit'
-import Diary from './pages/Diary'
+import Home from "./pages/Home";
+import New from "./pages/New";
+import Edit from "./pages/Edit";
+import Diary from "./pages/Diary";
 
 const reducer = (state, action) => {
   let newState = [];
   switch (action.type) {
-    case 'INIT': {
+    case "INIT": {
       return action.data;
     }
-    case 'CREATE': {
+    case "CREATE": {
       // const newItem = {
       //   ...action.data
       // };
@@ -22,20 +22,22 @@ const reducer = (state, action) => {
       // default까지 return 하지 않으려면 break걸어줘야함
       break;
     }
-    case 'REMOVE': {
+    case "REMOVE": {
       newState = state.filter((it) => it.id !== action.targetId);
       break;
     }
-    case 'EDIT': {
+    case "EDIT": {
       // content만 바꾸는게 아니라 전체를 바꿀거기 때문에
-      newState = state.map((it) => it.id === action.data.id ? { ...action.data } : it);
+      newState = state.map((it) =>
+        it.id === action.data.id ? { ...action.data } : it
+      );
       break;
     }
     default:
       return state;
   }
   return newState;
-}
+};
 
 export const DiaryStateContext = React.createContext();
 export const DiaryDispatchContext = React.createContext();
@@ -45,33 +47,33 @@ const dummyData = [
     id: 1,
     emotion: 1,
     content: "오늘의 일기 1번",
-    date : 1660205201524
+    date: 1660205201524,
   },
   {
     id: 2,
     emotion: 2,
     content: "오늘의 일기 2번",
-    date : 1660205201525
+    date: 1660205201525,
   },
   {
     id: 3,
     emotion: 3,
     content: "오늘의 일기 3번",
-    date : 1660205201526
+    date: 1660205201526,
   },
   {
     id: 4,
     emotion: 4,
     content: "오늘의 일기 4번",
-    date : 1660205201527
+    date: 1660205201527,
   },
   {
     id: 5,
     emotion: 5,
     content: "오늘의 일기 5번",
-    date : 1660205201528
-  }
-]
+    date: 1660205201528,
+  },
+];
 function App() {
   const [data, dispatch] = useReducer(reducer, dummyData);
   const dataId = useRef(0);
@@ -85,14 +87,14 @@ function App() {
         date: new Date(date).getTime(),
         content,
         emotion,
-      }
+      },
     });
     dataId.current += 1;
-  };  
+  };
   // REMOVE
   const onRemove = (targetId) => {
     dispatch({ type: "REMOVE", targetId });
-  }
+  };
   // EDIT
   // id만 유지하면서 다른이들은 변경
   const onEdit = (targetId, date, content, emotion) => {
@@ -106,23 +108,20 @@ function App() {
       },
     });
   };
-  
+
   return (
-    
     <DiaryStateContext.Provider value={data}>
-      <DiaryDispatchContext.Provider
-        value={{ onCreate, onEdit, onRemove }}
-      >
+      <DiaryDispatchContext.Provider value={{ onCreate, onEdit, onRemove }}>
         <BrowserRouter>
           <div className="App">
             <Routes>
               <Route path="/" element={<Home />} />
               <Route path="/new" element={<New />} />
-              <Route path="/edit" element={<Edit />} />
+              <Route path="/edit/:id" element={<Edit />} />
               <Route path="/diary/:id" element={<Diary />} />
             </Routes>
           </div>
-        </BrowserRouter>      
+        </BrowserRouter>
       </DiaryDispatchContext.Provider>
     </DiaryStateContext.Provider>
   );
--- emotion-diary/src/pages/Edit.js
+++ emotion-diary/src/pages/Edit.js
@@ -1,11 +1,32 @@
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { DiaryStateContext } from "../App";
+import DiaryEditor from "../components/DiaryEditor";
 
 const Edit = () => {
+  const [originData, setOriginData] = useState();
   const navigate = useNavigate();
+  const { id } = useParams();
 
+  const diaryList = useContext(DiaryStateContext);
+
+  useEffect(() => {
+    //   일기 데이터가 하나라도 있을때 들고오도록
+    if (diaryList.length >= 1) {
+      const targetDiary = diaryList.find(
+        (it) => parseInt(it.id) === parseInt(id)
+      );
+
+      if (targetDiary) {
+        setOriginData(targetDiary);
+      } else {
+        navigate("/", { replace: true });
+      }
+    }
+  }, [id, diaryList]);
   return (
     <div>
-      <h2>Edit</h2>
+      {originData && <DiaryEditor isEdit={true} originData={originData} />}
     </div>
   );
 };
